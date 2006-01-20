8e0b8ae418bd1d7db89a2a7bab723c210fa9f231
Tim Fletcher <twoggle@gmail.com>
2006-01-20 10:01:09 +0000

    test/test_markaby.rb: test capture and output_helpers

--- test/test_markaby.rb
+++ test/test_markaby.rb
@@ -7,6 +7,20 @@ class MarkabyTest < Test::Unit::TestCase
     Markaby::Template.new(string.to_s).render(assigns, helpers)
   end
   
+  def setup
+    @helpers = Module.new do
+      def link_to(obj)
+        %{<a href="">#{obj}</a>}
+      end
+      def pluralize(n, string)
+        n == 1 ? string : string + "s"
+      end
+      module_function :link_to, :pluralize
+    end
+    @helpers_ohf = @helpers.dup
+    @helpers_ohf.instance_variable_set("@output_helpers", false)
+  end
+
   def test_simple
     assert_equal "<hr/>\n", mab("hr")
     assert_equal "<p>foo</p>\n", mab("p 'foo'")
@@ -20,8 +34,20 @@ class MarkabyTest < Test::Unit::TestCase
   end
   
   def test_escaping
-    assert_equal %{<h1>Apples &amp; Oranges</h1>\n}, mab("h1 'Apples & Oranges'")
-    assert_equal %{<h1>\nApples & Oranges</h1>\n}, mab("h1 { 'Apples & Oranges' }")
+    assert_equal "<h1>Apples &amp; Oranges</h1>\n", mab("h1 'Apples & Oranges'")
+    assert_equal "<h1>\nApples & Oranges</h1>\n", mab("h1 { 'Apples & Oranges' }")
   end
-  
+
+  def test_capture
+    html = "<div>\n<h1>hello world</h1>\n</div>\n"
+    assert_equal html, mab("div { h1 'hello world' }")
+    assert_equal html, mab("div { capture { h1 'hello world' } }")
+    assert mab("capture { h1 'hello world' }").empty?
+  end
+
+  def test_output_helpers
+    assert_equal %{<a href="">edit</a>}, mab("link_to('edit')", {}, @helpers)
+    assert mab("link_to('edit')", {}, @helpers_ohf).empty?
+  end
+
 end
