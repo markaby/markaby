ffde03677d94b9232da94233115771640e77183f
Tim Fletcher <twoggle@gmail.com>
2006-01-25 19:30:23 +0000

    adding (ActionController) helper for rendering markaby inline

--- init.rb
+++ init.rb
@@ -2,5 +2,8 @@ $:.unshift File.expand_path(File.join(File.dirname(__FILE__), 'lib'))
 
 require 'markaby'
 require 'markaby/view'
+require 'markaby/helper'
 
 ActionView::Base::register_template_handler :mab, Markaby::View
+
+ActionController::Base.send :include, Markaby::ActionControllerHelper
