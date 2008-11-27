5b45794e71d856ad3e60a9d9b79221cbc8a54b73
John Barton (joho) <jrbarton@gmail.com>
2008-11-27 11:04:37 +1100

    fixing init process so it will work in rails 2.1/2.2 as well as the old stuff

--- init.rb
+++ init.rb
@@ -3,6 +3,11 @@ $:.unshift File.expand_path(File.join(File.dirname(__FILE__), 'lib'))
 require 'markaby'
 require 'markaby/rails'
 
-ActionView::Base::register_template_handler 'mab', Markaby::Rails::ActionViewTemplateHandler
+
+if defined? ActionView::Template and ActionView::Template.respond_to? :register_template_handler
+  ActionView::Template
+else
+  ActionView::Base
+end.register_template_handler(:mab, Markaby::Rails::ActionViewTemplateHandler)
 
 ActionController::Base.send :include, Markaby::Rails::ActionControllerHelpers
