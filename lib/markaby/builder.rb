283b8f00d2905d1cbecd7cdb90ed96f1d57b4e07
Scott Taylor <scott@railsnewbie.com>
2009-08-23 21:07:30 -0400

    Move html builder tags into a module.  Mix it into Markaby::Builder

--- lib/markaby/builder.rb
+++ lib/markaby/builder.rb
@@ -1,4 +1,5 @@
 require 'markaby/tags'
+require 'markaby/builder_tags'
 
 module Markaby
   # The Markaby::Builder class is the central gear in the system.  When using
@@ -19,6 +20,7 @@ module Markaby
   #   puts mab.to_s
   #
   class Builder
+    include Markaby::BuilderTags
 
     @@default = {
       :indent                 => 0,
@@ -165,43 +167,6 @@ module Markaby
       else
         tag!(sym, *args, &block)
       end
-    end
-
-    (XHTMLTransitional.tags - [:head]).each do |k|
-      class_eval %{
-        def #{k}(*args, &block)
-          html_tag(#{k.inspect}, *args, &block)
-        end
-      }
-    end
-
-    # Builds a head tag.  Adds a <tt>meta</tt> tag inside with Content-Type
-    # set to <tt>text/html; charset=utf-8</tt>.
-    def head(*args, &block)
-      tag!(:head, *args) do
-        tag!(:meta, "http-equiv" => "Content-Type", "content" => "text/html; charset=utf-8") if @output_meta_tag
-        instance_eval(&block)
-      end
-    end
-
-    # Builds an html tag.  An XML 1.0 instruction and an XHTML 1.0 Transitional doctype
-    # are prepended.  Also assumes <tt>:xmlns => "http://www.w3.org/1999/xhtml",
-    # :lang => "en"</tt>.
-    def xhtml_transitional(attrs = {}, &block)
-      self.tagset = Markaby::XHTMLTransitional
-      xhtml_html(attrs, &block)
-    end
-
-    # Builds an html tag with XHTML 1.0 Strict doctype instead.
-    def xhtml_strict(attrs = {}, &block)
-      self.tagset = Markaby::XHTMLStrict
-      xhtml_html(attrs, &block)
-    end
-
-    # Builds an html tag with XHTML 1.0 Frameset doctype instead.
-    def xhtml_frameset(attrs = {}, &block)
-      self.tagset = Markaby::XHTMLFrameset
-      xhtml_html(attrs, &block)
     end
 
   private
---
+++ lib/markaby/builder_tags.rb
@@ -0,0 +1,40 @@
+module Markaby
+  module BuilderTags
+    (XHTMLTransitional.tags - [:head]).each do |k|
+      class_eval %{
+        def #{k}(*args, &block)
+          html_tag(#{k.inspect}, *args, &block)
+        end
+      }
+    end
+
+    # Builds a head tag.  Adds a <tt>meta</tt> tag inside with Content-Type
+    # set to <tt>text/html; charset=utf-8</tt>.
+    def head(*args, &block)
+      tag!(:head, *args) do
+        tag!(:meta, "http-equiv" => "Content-Type", "content" => "text/html; charset=utf-8") if @output_meta_tag
+        instance_eval(&block)
+      end
+    end
+
+    # Builds an html tag.  An XML 1.0 instruction and an XHTML 1.0 Transitional doctype
+    # are prepended.  Also assumes <tt>:xmlns => "http://www.w3.org/1999/xhtml",
+    # :lang => "en"</tt>.
+    def xhtml_transitional(attrs = {}, &block)
+      self.tagset = Markaby::XHTMLTransitional
+      xhtml_html(attrs, &block)
+    end
+
+    # Builds an html tag with XHTML 1.0 Strict doctype instead.
+    def xhtml_strict(attrs = {}, &block)
+      self.tagset = Markaby::XHTMLStrict
+      xhtml_html(attrs, &block)
+    end
+
+    # Builds an html tag with XHTML 1.0 Frameset doctype instead.
+    def xhtml_frameset(attrs = {}, &block)
+      self.tagset = Markaby::XHTMLFrameset
+      xhtml_html(attrs, &block)
+    end
+  end
+end
