8f219bb951606f0b546a546814afa50a4846a9ea
Scott Taylor <scott@railsnewbie.com>
2009-08-29 18:39:16 -0400

    Only run rails tests when inside a rails project

--- test/rails/preamble.rb
+++ test/rails/preamble.rb
@@ -1,30 +1,29 @@
 require 'test/unit'
 
-MARKABY_ROOT = File.join(File.dirname(__FILE__), '..', '..')
+MARKABY_ROOT    = File.join(File.dirname(__FILE__), "..", "..")
+rails_root      = File.join(MARKABY_ROOT, "..", "..", "..")
+RAILS_BOOT_FILE = File.join(rails_root, "config", "boot")
 
-require File.join(MARKABY_ROOT,
-    '..', # plugins/
-    '..', # vendor/
-    '..', # RAILS_ROOT
-    'config',
-    'boot'
-  )
+RUNNING_RAILS = File.exists?(RAILS_BOOT_FILE) ? true : false
 
-Rails::Initializer.run
+if RUNNING_RAILS
+  require RAILS_BOOT_FILE
 
-require 'action_controller/test_process'
+  Rails::Initializer.run
 
-Dependencies.load_paths.unshift File.dirname(__FILE__)
+  require 'action_controller/test_process'
 
-$:.unshift MARKABY_ROOT
+  Dependencies.load_paths.unshift File.dirname(__FILE__)
 
-require 'init'
+  $:.unshift markaby_root
 
-require 'markaby/kernel_method'
+  require 'init'
 
+  require 'markaby/kernel_method'
 
-class Monkey < Struct.new(:name)
-  def self.find(*args)
-    @@monkeys ||= ['Frank', 'Benny', 'Paul'].map { |name| Monkey.new name }
+  class Monkey < Struct.new(:name)
+    def self.find(*args)
+      @@monkeys ||= ['Frank', 'Benny', 'Paul'].map { |name| Monkey.new name }
+    end
   end
 end
