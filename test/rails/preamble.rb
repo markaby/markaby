58ebd2f52ffc1d5afb51cb676fa8ad1ac42d53f4
Scott Taylor <scott@railsnewbie.com>
2009-09-16 02:59:05 -0400

    Get rails 2.3.2 support going.  Neglect other versions for the time being

--- test/rails/preamble.rb
+++ test/rails/preamble.rb
@@ -12,20 +12,21 @@ if RUNNING_RAILS
   Rails::Initializer.run
 
   require 'action_controller/test_process'
-
-  Dependencies.load_paths.unshift File.dirname(__FILE__)
+  
+  if defined?(Dependencies)
+    Dependencies.load_paths.unshift File.dirname(__FILE__)
+  end
 
   $:.unshift MARKABY_ROOT
 
   require 'init'
-
   require 'markaby/kernel_method'
-
-  class Monkey < Struct.new(:name)
-    def self.find(*args)
-      @@monkeys ||= ['Frank', 'Benny', 'Paul'].map { |name| Monkey.new name }
-    end
-  end
 else
   warn "Skipping rails specific tests"
+end
+
+class Monkey < Struct.new(:name)
+  def self.find(*args)
+    @@monkeys ||= ['Frank', 'Benny', 'Paul'].map { |name| Monkey.new name }
+  end
 end
--- test/test_builder.rb
+++ test/test_builder.rb
@@ -13,6 +13,19 @@ class BuilderTest < Test::Unit::TestCase
     assert Markaby::Builder.private_instance_methods.include?("method_missing")
   end
   
+  # setting options
+  def test_should_be_able_to_restore_defaults_after_setting
+    Markaby::Builder.set :indent, 2
+    Markaby::Builder.restore_defaults!
+    
+    assert_equal 0, Markaby::Builder.get(:indent)
+  end
+  
+  def test_should_be_able_set_global_options
+    Markaby::Builder.set :indent, 2
+    assert_equal 2, Markaby::Builder.get(:indent)
+  end
+  
   # internal clobbering by passed in assigns
   def test_internal_helpers_ivar_should_not_be_overwritten_by_assigns
     helper = Class.new do
