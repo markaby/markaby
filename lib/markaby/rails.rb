e5299ca3aa287301322517b45e84ecdc07e3def3
Scott Taylor <scott@railsnewbie.com>
2009-09-16 03:46:58 -0400

    Remove pre rails-2.0.1 support files

--- lib/markaby/rails.rb
+++ lib/markaby/rails.rb
@@ -1,7 +1,29 @@
 if defined?(Rails)
-  if Rails::VERSION::STRING == "2.3.2"
-    require File.dirname(__FILE__) + "/rails/2.3.2.rb"
-  else
-    require "markaby/rails/pre_2_0_2"
+  module Markaby
+    module Rails
+      class TemplateHandler < ::ActionView::TemplateHandler
+        def render(template, local_assigns={})
+          builder = Markaby::Builder.new(instance_variables.merge(local_assigns), @view)
+          builder.instance_eval(template.source)
+          builder.to_s
+        end
+
+      private
+
+        def instance_variables
+          instance_variable_hash(@view)
+        end
+
+        def instance_variable_hash(object)
+          returning Hash.new do |hash|
+            object.instance_variables.each do |var_name|
+              hash[var_name.gsub("@", "")] = object.instance_variable_get(var_name)
+            end
+          end
+        end
+      end
+    end
   end
+
+  ActionView::Template.register_template_handler(:mab, Markaby::Rails::TemplateHandler)
 end
