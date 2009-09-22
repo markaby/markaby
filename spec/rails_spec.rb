ce215fcb5cc0913b284754bc9e4f7110efaf4368
Scott Taylor <scott@railsnewbie.com>
2009-09-22 04:22:27 -0400

    Fix template specs which were previously pending

--- spec/rails_spec.rb
+++ spec/rails_spec.rb
@@ -1,81 +1,80 @@
 require File.join(File.dirname(__FILE__), 'rails', 'spec_helper')
 
-class TestController < ActionController::Base
-  VIEW_PATH = File.expand_path(File.join(File.dirname(__FILE__), 'rails', 'views'))
-  append_view_path(VIEW_PATH)
+if RUNNING_RAILS
+  class TestController < ActionController::Base
+    VIEW_PATH = File.expand_path(File.join(File.dirname(__FILE__), 'rails', 'views'))
+    append_view_path(VIEW_PATH)
 
-  around_filter :catch_exceptions
+    around_filter :catch_exceptions
 
-  attr_reader :last_exception
+    attr_reader :last_exception
 
-  def catch_exceptions
-    yield
-  rescue Exception => @last_exception
-    raise @last_exception
-  end
-end
-
-class MarkabyController < TestController
-  def renders_nothing
-    render :text => ""
+    def catch_exceptions
+      yield
+    rescue Exception => @last_exception
+      raise @last_exception
+    end
   end
 
-  def renders_erb_with_explicit_template
-    render :template => "markaby/renders_erb"
-  end
-
-  def render_inline_content
-    mab_content = mab do
-      ul do
-        li "Scott"
-      end
+  class MarkabyController < TestController
+    def renders_nothing
+      render :text => ""
     end
 
-    render :inline => mab_content
+    def renders_erb_with_explicit_template
+      render :template => "markaby/renders_erb"
+    end
+
+    def render_inline_content
+      mab_content = mab do
+        ul do
+          li "Scott"
+        end
+      end
+
+      render :inline => mab_content
+    end
+
+    def render_explicit_but_empty_markaby_layout
+      render :template => "markaby/render_explicit_but_empty_markaby_layout.mab"
+    end
+
+    def no_values_passed
+      render :template => "markaby/no_values_passed"
+    end
+
+    def correct_template_values
+      render :template => "markaby/correct_template_values"
+    end
+
+    def render_erb_without_explicit_render_call
+    end
+
+    def render_mab_without_explicit_render_call
+    end
+
+    def render_with_ivar
+      @user = "smtlaissezfaire"
+      render :template => "markaby/render_with_ivar"
+    end
+
+    def access_to_helpers
+      render :template => "markaby/access_to_helpers"
+    end
+
+    def renders_partial
+      render :template => "markaby/partial_parent"
+    end
+
+    def renders_partial_with_locals
+      render :template => "markaby/partial_parent_with_locals"
+    end
+
+    def render_which_raises_error
+      render :template => "markaby/broken"
+    end
   end
 
-  def render_explicit_but_empty_markaby_layout
-    render :template => "markaby/render_explicit_but_empty_markaby_layout.mab"
-  end
-
-  def no_values_passed
-    render :template => "markaby/no_values_passed"
-  end
-
-  def correct_template_values
-    render :template => "markaby/correct_template_values"
-  end
-
-  def render_erb_without_explicit_render_call
-  end
-
-  # TODO: See test below
-  # def render_without_explicit_render_call
-  # end
-
-  def render_with_ivar
-    @user = "smtlaissezfaire"
-    render :template => "markaby/render_with_ivar"
-  end
-
-  def access_to_helpers
-    render :template => "markaby/access_to_helpers"
-  end
-
-  def renders_partial
-    render :template => "markaby/partial_parent"
-  end
-
-  def renders_partial_with_locals
-    render :template => "markaby/partial_parent_with_locals"
-  end
-
-  def render_which_raises_error
-    render :template => "markaby/broken"
-  end
-end
-
-if RUNNING_RAILS
   class MarkabyOnRailsTest < ActionController::TestCase
     def setup
       Markaby::Builder.restore_defaults!
@@ -126,24 +125,19 @@ if RUNNING_RAILS
       assert_equal "<ul><li>smt</li><li>joho</li><li>spox</li></ul>", @response.body
     end
 
-    # TODO:
-    #   Fix the following test environment issue so that the following
-    #   test will run
-    #
-    #
-    # def test_renders_default_erb_without_explicit_render_call
-    #   get :render_erb_without_explicit_render_call
-    #   assert_response :success
-    #   
-    #   assert_equal "hello, from erb"
-    # end
-    # 
-    # def test_renders_without_explicit_render_call
-    #   get :render_without_explicit_render_call
-    #   assert_response :success
-    #   
-    #   assert_equal "<ul><li>smtlaissezfaire</li></ul>", @response.body
-    # end
+    def test_renders_default_erb_without_explicit_render_call
+      get :render_erb_without_explicit_render_call
+      assert_response :success
+      
+      assert_equal @response.body, "hello, from erb"
+    end
+    
+    def test_renders_without_explicit_render_call
+      get :render_mab_without_explicit_render_call
+      assert_response :success
+      
+      assert_equal @response.body, "<ul><li>smtlaissezfaire</li></ul>"
+    end
 
     def test_renders_with_ivar
       get :render_with_ivar
