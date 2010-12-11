98e6a42f364a83f50b3ae11c60ae86fbea78a8f3
Aria Stewart <aredridel@nbtsc.org>
2010-12-11 06:26:52 +0100

    Add rudimentary HTML5 tagset

--- lib/markaby/tags.rb
+++ lib/markaby/tags.rb
@@ -190,4 +190,38 @@ module Markaby
     @forms = @tags & FORM_TAGS
     @self_closing = @tags & SELF_CLOSING_TAGS
   end
+
+  class HTML5
+    class << self
+      attr_accessor :tags, :tagset, :forms, :self_closing, :doctype
+    end
+
+    @doctype = ['html']
+    @tagset = XHTMLTransitional.tagset.merge({
+        :abbr => Attrs,
+        :article => Attrs,
+        :aside => Attrs,
+        :audio => Attrs,
+        :canvas => Attrs,
+        :datalist => Attrs,
+        :details => Attrs,
+        :figure => Attrs,
+        :footer => Attrs,
+        :header => Attrs,
+        :hgroup => Attrs,
+        :mark => Attrs,
+        :menu => Attrs,
+        :meter => Attrs,
+        :nav => Attrs,
+        :output => Attrs,
+        :progress => Attrs,
+        :section => Attrs,
+        :time => Attrs,
+        :video => Attrs
+    })
+
+    @tags = @tagset.keys
+    @forms = @tags & FORM_TAGS
+    @self_closing = @tags & SELF_CLOSING_TAGS
+  end
 end
