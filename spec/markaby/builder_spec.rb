b0b84d415e13075be0d828d76ed08c70835c3521
Scott Taylor <scott@railsnewbie.com>
2018-01-07 12:53:28 -0800

    spec cleanup

--- spec/markaby/builder_spec.rb
+++ spec/markaby/builder_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 
 module Markaby
-  describe Markaby::Builder do
+  describe Builder do
     before do
       Markaby::Builder.restore_defaults!
     end
